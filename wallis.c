8b8332bf3f903aae1990880e640eae6f762d853b
Malavika-R <66917713+Malavika-R@users.noreply.github.com>
2021-06-03 21:47:10 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -5,9 +5,9 @@
 
 float wallis_pi(int n)
   { float pi=1.0;
-  for(int i=1; i<=n; i++)
+  for(int i=1;i<=n;i++)
   {
-    pi*=((4.0*i*i)/(float)((4.0*i*i)-1));
+    pi*=(4.0*i*i)/(float)((4.0*i*i)-1));
   }
   return 2.0*pi;
 }
